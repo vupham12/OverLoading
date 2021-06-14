81c9453c2c4d90b164bbf918208d58f41aa91f03
vupham12 <82140145+vupham12@users.noreply.github.com>
2021-06-14 16:55:20 +0700

    first commit

---
+++ src/advance/dev/MainApp.java
@@ -0,0 +1,14 @@
+package advance.dev;
+
+import java.util.Scanner;
+
+public class MainApp {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Hello Word");
+
+	}
+
+}
---
+++ src/module-info.java
@@ -0,0 +1,2 @@
+module OverLoading {
+}
